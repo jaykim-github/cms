d9016b4855101b6687b86c22456a8515145b15a9
jaykim-github <jaykeem713@gmail.com>
2022-12-31 02:17:04 +0900

    feat : [001-EMAIL-Connect] mailgun에 대한 email 연동
    무료 계정으로 테스트 시 이메일 등록 반드시 필요

---
+++ src/main/java/com/zerobase/cms/user/UserApiApplication.java
@@ -0,0 +1,22 @@
+package com.zerobase.cms.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+@EnableFeignClients
+@SpringBootApplication
+@RequiredArgsConstructor
+@EnableJpaAuditing
+@EnableJpaRepositories
+public class UserApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApiApplication.class, args);
+    }
+
+}
